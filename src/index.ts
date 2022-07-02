ee882b224bf3153d4b98fab59360ded3d92ab094
Zach Schneider <hello@zacharyschneider.ca>
2022-07-02 15:05:59 -0700

    routes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,27 @@
-import { Router } from 'itty-router'
+import { RouteHandler, Router, Request as IRequest } from 'itty-router'
 import { cors } from '@/cors'
 import packageJson from '../package.json'
 
-import { getWeatherData } from "@/routes/weather"
+import { getWateringData, getWeatherData } from "@/routes/weather"
 import { getBaselineETo } from "@/routes/baselineETo"
 
-export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-}
 
 const router = Router();
 
 // Handle requests to apex. Return 503 status to keep crawlers away.
 router.get('/', () => new Response(`${packageJson.description} v${packageJson.version}`, { status: 503 }))
 
+router.routes.push([
+	'GET',
+	/^\/(?<method>\d+)\/?$/,
+	[getWateringData as RouteHandler<IRequest>]
+])
+
+// This route is for the GUI
 router.options('/weatherData', cors())
 router.get('/weatherData', getWeatherData)
 
+// This route is for the GUI
 router.options('/baselineETo', cors())
 router.get('/baselineETo', getBaselineETo)
 
