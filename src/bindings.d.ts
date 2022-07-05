66987f573388d476e4ad73ba012f12a75cfba3b8
Zach Schneider <hello@zacharyschneider.ca>
2022-07-05 01:33:34 -0700

    types

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -1,27 +1,29 @@
 import { BaselineEToStorage, GeocoderService, GeocoderCache, TimeZoneLookupService, WateringScaleCache, WeatherProvider } from '@/constants';
 
-export interface Env {
-	/* Baseline ETo */
-	BASELINE_ETO: BaselineEToStorage
-	BASELINE_ETO_R2_BUCKET: R2Bucket
-	BASELINE_ETO_R2_PATH?: string
+declare global {
+	export interface Env {
+		/* Baseline ETₒ */
+		BASELINE_ETO: BaselineEToStorage
+		BASELINE_ETO_R2_BUCKET: R2Bucket
+		BASELINE_ETO_R2_PATH?: string
 
-	/* Weather */
-	WEATHER_PROVIDER: WeatherProvider
+		/* Weather */
+		WEATHER_PROVIDER: WeatherProvider
 
-	/* Geocoder */
-	GEOCODER: GeocoderService
-	GEOCODER_CACHE: GeocoderCache
-	GEOCODER_CACHE_KV: KVNamespace
+		/* Geocoder */
+		GEOCODER: GeocoderService
+		GEOCODER_CACHE: GeocoderCache
+		GEOCODER_CACHE_KV: KVNamespace
 
-	/* Timezone */
-	TIMEZONE_ID?: string
-	TIMEZONE_LOOKUP: TimeZoneLookupService
+		/* Timezone */
+		TIMEZONE_ID?: string
+		TIMEZONE_LOOKUP: TimeZoneLookupService
 
-	/* Caching */
-	WATERING_SCALE_CACHE: WateringScaleCache
+		/* Caching */
+		WATERING_SCALE_CACHE: WateringScaleCache
 
-	/* API Keys */
-	OWM_API_KEY?: string
-	GOOGLE_MAPS_API_KEY?: string
+		/* API Keys */
+		OWM_API_KEY?: string
+		GOOGLE_MAPS_API_KEY?: string
+	}
 }
